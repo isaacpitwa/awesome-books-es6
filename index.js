8f59b9b4d383944ea6c0a3a8f7d65adbb59a8273
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-23 01:54:49 +0300

    Add Luxon for realtime

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Methods  } from "./modules/functions.js";
 import {listActive,contactActive,formActive} from './modules/navigation.js'
 import {displayBooks,addBook,getData} from './modules/books.js'
+import  {luxon} from "./modules/luxon.js";
 
 const methods = new Methods();
 methods.books = [];
@@ -10,6 +11,14 @@ window.onbeforeunload = () => {
   displayBooks(methods);
 };
 window.onload = listActive;
+
+// Date
+const setTime =()=>{
+  const dateContainer = document.getElementById("date")
+  dateContainer.innerText = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_FULL)
+}
+setInterval(setTime, 1000);
+
 /* form functions */
 
 const addBtn = document.querySelector('#add-btn');
